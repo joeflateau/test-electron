3d60bb75b1bd9d8c604f958a5736f7b6a5436311
Joe Flateau <joe@joeflateau.net>
2021-05-27 09:40:58 -0400

    only sign if a password is provided

--- forge.config.js
+++ forge.config.js
@@ -1,26 +1,30 @@
 module.exports = {
   packagerConfig: {
-    osxSign: {
-      identity: "Developer ID Application: Upstoric LLC (5A46AL2PS3)",
-      "hardened-runtime": true,
-      entitlements: "entitlements.plist",
-      "entitlements-inherit": "entitlements.plist",
-      "signature-flags": "library",
-      "gatekeeper-assess": false,
-    },
-    osxNotarize: {
-      appleId: "joe@upstoric.com",
-      appleIdPassword: process.env.APP_STORE_CONNECT_PASSWORD,
-    },
+    ...(process.env.APP_STORE_CONNECT_PASSWORD && {
+      osxSign: {
+        identity: "Developer ID Application: Upstoric LLC (5A46AL2PS3)",
+        "hardened-runtime": true,
+        entitlements: "entitlements.plist",
+        "entitlements-inherit": "entitlements.plist",
+        "signature-flags": "library",
+        "gatekeeper-assess": false,
+      },
+      osxNotarize: {
+        appleId: "joe@upstoric.com",
+        appleIdPassword: process.env.APP_STORE_CONNECT_PASSWORD,
+      },
+    }),
   },
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {
         name: "test_electron",
-        certificateFile: "./cert.p12",
-        certificatePassword: process.env.WINDOWS_CERT_PASSWORD,
-        signWithParams: "/t http://timestamp.digicert.com",
+        ...(process.env.WINDOWS_CERT_PASSWORD && {
+          certificateFile: "./cert.p12",
+          certificatePassword: process.env.WINDOWS_CERT_PASSWORD,
+          signWithParams: "/t http://timestamp.digicert.com",
+        }),
         noMsi: false,
       },
     },
